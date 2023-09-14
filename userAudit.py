2f68a0e903b6e4d5ed964cd8c408b8618a5e6769
Nathan Wing <118492887+nathanawing@users.noreply.github.com>
2023-09-14 11:13:30 -0700

    Update userAudit.py

--- userAudit.py
+++ userAudit.py
@@ -7,15 +7,18 @@ temp.close()
 shadowList = file.splitlines()
 passList = file2.splitlines()
 numUsers = len(shadowList)
+
 # Count locked accounts
 disabled = file.count(':*:')
 print("There are " + str(numUsers) + " total accounts")
 print(str(disabled) + " accounts are disabled")
 locked = file.count(':!:')
 print(str(locked) + " accounts are locked")
+
 # Count accounts without passwords
 noPass = file.count(':!!:')
 print(str(noPass) + " accounts are locked without passwords")
+
 # Count accounts with passwords
 passwords = file.count(':$6$')
 print(str(passwords) + " accounts have passwords")
@@ -23,16 +26,19 @@ noExpire = 0
 expired = 0
 for i in range(numUsers):
     userShad = shadowList[i].split(':')
+    
 # Check for days til password expiration
     try:
         if int(userShad[4]) > 90:
             print(userShad[0] + "'s password expires after more than 90 days")
             noExpire += 1
+            
 # Exception for blank expiration date
     except ValueError:
         print(userShad[0] + "'s password does not have an expiration date")
         noExpire += 1
 print(str(noExpire) + " account passwords do not expire")
+
 # Check for expired accounts
 for i in range(numUsers):
     userShad = shadowList[i].split(':')
@@ -43,6 +49,7 @@ if expired > 0:
     print(str(expired) + " account passwords have expired")
 else:
     print("No accounts are expired")
+    
 # Check user accounts without passwords
 for i in range(numUsers):
     userShad = shadowList[i].split(':')
